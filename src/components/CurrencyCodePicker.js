b3ef2d9c8a296ee09217812481c157d353cb1a00
Pham Huu Hien <phhien203@gmail.com>
2023-08-19 11:32:19 +0700

    convert CurrencyCodePicker to function component

--- src/components/CurrencyCodePicker.js
+++ src/components/CurrencyCodePicker.js
@@ -1,43 +1,38 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import {
   getCurrencyCode,
   getSupportedCurrencies,
-} from "../store/reducers/RateReducer";
-import { updateCurrencyCode } from "../store/actions/RateActions";
+} from '../store/reducers/RateReducer';
+import { updateCurrencyCode } from '../store/actions/RateActions';
 
-export class CurrencyCodePicker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onChange = this.onChange.bind(this);
-  }
-  onChange(e) {
+export function CurrencyCodePicker({
+  currencyCode,
+  supportedCurrencies,
+  currencyCodeUpdate,
+}) {
+  function onChange(e) {
     const currencyCode = e.target.value;
-    this.props.currencyCodeUpdate(currencyCode);
+    currencyCodeUpdate(currencyCode);
   }
-  render() {
-    const { currencyCode, supportedCurrencies } = this.props;
-    return (
-      <select
-        className="currencyCode"
-        value={currencyCode}
-        onChange={this.onChange}
-      >
-        {supportedCurrencies.map((code) => (
-          <option key={code} value={code}>
-            {code}
-          </option>
-        ))}
-      </select>
-    );
-  }
+
+  return (
+    <select className="currencyCode" value={currencyCode} onChange={onChange}>
+      {supportedCurrencies.map((code) => (
+        <option key={code} value={code}>
+          {code}
+        </option>
+      ))}
+    </select>
+  );
 }
 
 // prop types
 CurrencyCodePicker.propTypes = {
-  supportedCurrencies: PropTypes.arrayOf(PropTypes.string),
   currencyCode: PropTypes.string,
+  supportedCurrencies: PropTypes.arrayOf(PropTypes.string),
+  currencyCodeUpdate: PropTypes.func,
 };
 
 // redux stuff
