bbbcee31a2330f81ea516a5cd613529394f9f5c6
Pham Huu Hien <phhien203@gmail.com>
2023-08-19 11:52:20 +0700

    migrate RateTable to function component

--- src/components/RateTable.js
+++ src/components/RateTable.js
@@ -1,64 +1,60 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import Bounce from "bounce.js";
-import { getAmount, getRates } from "../store/reducers/RateReducer";
-import { getName } from "../store/reducers/UserReducer";
+import Bounce from 'bounce.js';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
 
-export class RateTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.nameRef = React.createRef();
-    this.bounce = this.bounce.bind(this);
-  }
-  render() {
-    const { rates, amount, name } = this.props;
-    return (
-      <table className="ExchangeRate-table">
-        <tbody>
-          {Object.entries(rates).map(([code, rate]) => {
-            // NOTE: normally avoid floating point math in JS
-            const exchangeAmount = amount * rate || 0.0;
-            return (
-              <tr key={code}>
-                <td>{code}</td>
-                <td>
-                  {exchangeAmount.toLocaleString("en", {
-                    style: "currency",
-                    currency: code,
-                  })}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan={2} onMouseEnter={this.bounce} ref={this.nameRef}>
-              Prepared for {name}
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-    );
-  }
-  bounce() {
+import { getAmount, getRates } from '../store/reducers/RateReducer';
+import { getName } from '../store/reducers/UserReducer';
+
+export function RateTable({ rates, amount, name }) {
+  const nameRef = React.useRef();
+
+  function bounce() {
     var bounce = new Bounce();
     bounce
       .translate({
         from: { x: 0, y: 0 },
         to: { x: 0, y: 10 },
-        easing: "bounce",
+        easing: 'bounce',
         loop: true,
       })
       .scale({
         from: { x: 1, y: 1 },
         to: { x: 5, y: 1 },
-        easing: "sway",
+        easing: 'sway',
         duration: 1900,
       });
-    bounce.applyTo(this.nameRef.current);
+    bounce.applyTo(nameRef.current);
   }
+
+  return (
+    <table className="ExchangeRate-table">
+      <tbody>
+        {Object.entries(rates).map(([code, rate]) => {
+          // NOTE: normally avoid floating point math in JS
+          const exchangeAmount = amount * rate || 0.0;
+          return (
+            <tr key={code}>
+              <td>{code}</td>
+              <td>
+                {exchangeAmount.toLocaleString('en', {
+                  style: 'currency',
+                  currency: code,
+                })}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan={2} onMouseEnter={bounce} ref={nameRef}>
+            Prepared for {name}
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+  );
 }
 
 // prop types
